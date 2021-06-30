dbb29270f5f90443d35290b1d73e35a049073472
Konstantin Albul <konstantin.albul@sparm.com>
2021-06-30 22:39:20 +0300

    try #1

--- flexile-app/flexile-app/settings.py
+++ flexile-app/flexile-app/settings.py
@@ -25,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+if SECRET_KEY is None:
+    SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
